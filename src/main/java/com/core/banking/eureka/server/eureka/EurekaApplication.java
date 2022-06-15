a7291474b4ccc9c0d1506d083d46e65579061ae8
kwickramasinghe <kwickramasinghe001@gmail.com>
2022-06-15 14:09:01 +0530

    Test push

--- src/main/java/com/core/banking/eureka/server/eureka/EurekaApplication.java
+++ src/main/java/com/core/banking/eureka/server/eureka/EurekaApplication.java
@@ -8,6 +8,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaApplication {
 
+	//test push
+
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
 	}
